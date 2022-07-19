bb1ee700414736cef8d66bd706c22848f9be8396
anand <anand@Happy>
2022-07-19 10:27:56 +0530

    [anand]created by anand

---
+++ src/gitproject1/sample.java
@@ -0,0 +1,12 @@
+package gitproject1;
+
+public class sample {
+	
+	 public static void main(String[] args) {
+			
+    	 System.out.println("hellow");
+    	 System.out.println("hgcjii");
+    	 System.out.println("haii");
+	}
+
+}
